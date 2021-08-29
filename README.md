d7c431d772d5d6919b53d48893a36eb85d1a0e2c
Pascal Helfenstein <pascal.helfenstein@gmail.com>
2021-08-29 16:15:21 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+M3U File with Replay Support for TV7 from Init7
+===============================================
+
+This is an inofficial M3U File to support Replay TV with TV7 on Kodi using the Simple IPTV PVR Client.
+
+
+# Build the file
+    npm install
+    npm run build
---
+++ m3u-generator.js
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch';
+import fs from 'fs';
+
+const channelURL = 'https://tv7api2.tv.init7.net/api/tvchannel/';
+const m3uURL = 'https://api.init7.net/tvchannels.m3u';
+
+const channelsResponse = await fetch(channelURL);
+const channels = await channelsResponse.json();
+
+const m3uResponse = await fetch(m3uURL);
+const m3u = await m3uResponse.text();
+
+const parts = m3u.split(`\n`);
+for (let i = 1; i < parts.length; i+=2) {
+  const channel = channels.results.find(channel => channel.src == parts[i+1]);
+  if (channel && channel.has_replay) {
+    parts[i] = parts[i].replace(
+      '#EXTINF:0',
+      `#EXTINF:0 catchup="default" catchup-source="https://tv7api2.tv.init7.net/api/replay/?channel=${channel.pk}&start={utc:Y-m-dTH:M:S}Z&stop={utcend:Y-m-dTH:M:SZ}" catchup-days="7",`
+    )
+  }
+}
+
+fs.writeFileSync('TV7-replay.m3u', parts.join('\n'));
+
+console.log('M3U File written');
