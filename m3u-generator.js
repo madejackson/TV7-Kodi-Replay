98c56716de8e3764b30325d28fd69c5a3c427bfd
Pascal Helfenstein <pascal.helfenstein@gmail.com>
2021-09-04 23:55:06 +0200

    use api for m3u with hls_src

--- m3u-generator.js
+++ m3u-generator.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 import fs from 'fs';
 
 const channelURL = 'https://tv7api2.tv.init7.net/api/tvchannel/';
-const m3uURL = 'https://api.init7.net/tvchannels.m3u';
+const m3uURL = 'https://tv7api2.tv.init7.net/api/playlist/default.m3u?rp=true';
 
 const channelsResponse = await fetch(channelURL);
 const channels = await channelsResponse.json();
@@ -12,13 +12,12 @@ const m3u = await m3uResponse.text();
 
 const parts = m3u.split(`\n`);
 for (let i = 1; i < parts.length; i+=2) {
-  const channel = channels.results.find(channel => channel.src == parts[i+1]);
+  const channel = channels.results.find(channel => channel.hls_src == parts[i+1]);
   if (channel && channel.has_replay) {
     parts[i] = parts[i].replace(
       '#EXTINF:0',
       `#EXTINF:0 catchup="default" catchup-source="https://tv7api2.tv.init7.net/api/replay/?channel=${channel.pk}&epg_pk={catchup-id}" catchup-days="7",`
     )
-    parts[i+1] = channel.hls_src
   }
 }
 
