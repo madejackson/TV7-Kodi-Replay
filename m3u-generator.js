bfec7ee412066d5cd14556a0aa413033d4dfcaca
madejackson <martinra1994@gmail.com>
2021-12-16 16:00:38 +0100

    catch-up-support for TVirl, multicast for liveTV

--- m3u-generator.js
+++ m3u-generator.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 import fs from 'fs';
 
 const channelURL = 'https://tv7api2.tv.init7.net/api/tvchannel/';
-const m3uURL = 'https://tv7api2.tv.init7.net/api/playlist/default.m3u?rp=true';
+const m3uURL = 'https://tv7api2.tv.init7.net/api/playlist/default.m3u?rp=false';
 
 const channelsResponse = await fetch(channelURL);
 const channels = await channelsResponse.json();
@@ -12,11 +12,11 @@ const m3u = await m3uResponse.text();
 
 const parts = m3u.split(`\n`);
 for (let i = 1; i < parts.length; i+=2) {
-  const channel = channels.results.find(channel => channel.hls_src == parts[i+1]);
+  const channel = channels.results.find(channel => channel.src == parts[i+1]);
   if (channel && channel.has_replay) {
     parts[i] = parts[i].replace(
       '#EXTINF:0',
-      `#EXTINF:0 catchup="default" catchup-source="https://tv7api2.tv.init7.net/api/replay/?channel=${channel.pk}&epg_pk={catchup-id}" catchup-days="7",`
+      `#EXTINF:0 catchup="default" catchup-source="https://tv7api2.tv.init7.net/api/replay/?channel=${channel.pk}&start={utc:Y-m-dTH:M:S}Z&stop={utcend:Y-m-dTH:M:SZ}" catchup-days="7",`
     )
   }
 }
